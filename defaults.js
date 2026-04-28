d14001aa22814bee541a2e783e43095452b9817b
Ali Shahin <alishahin6787@gmail.com>
2026-04-28 18:32:20 +0300

    Delete defaults.js

--- defaults.js
+++
@@ -1,6 +0,0 @@
-// This file contains the default data for the application when hosted.
-// You can overwrite this file by clicking "Save as defaults.js" in the app.
-
-window.DEFAULT_ACCOUNTS = null;
-window.DEFAULT_OPENING_BALANCES = null;
-window.DEFAULT_JOURNAL = null;
